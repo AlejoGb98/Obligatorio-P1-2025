c204bf1ced643061ed4316117db6ef847ba154f2
AlejoGb98 <gallandalejo@gmail.com>
2025-06-08 20:08:17 -0300

    agregamos login y signup

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,88 @@
 window.addEventListener('load', inicio);
 let system = new Sistema()
 
+let logged = false;
+
 function inicio(){
-    document.querySelector('#loginBtn').addEventListener('click', funcLogin);
-} 
+    ocultarSecciones();
+    document.querySelector("#logout").style.display = 'none';
 
-function funcLogin(){
-    let user = document.querySelector('#txtUser').value;
-    let pass = document.querySelector('#txtPass').value;
 
-    system.login(user, pass);
-    
+    document.querySelector('#btnLogin').addEventListener('click', funcLogin);
+    document.querySelector('#btnSignup').addEventListener('click', funcSignup);
+    //document.querySelector("#btnLogout").addEventListener('click', logOut);
+
+
+    document.querySelector("#login").style.display = 'block';
+    document.querySelector("#btnToLogin").addEventListener('click', funcSwitchLog);
+    document.querySelector("#btnToSignup").addEventListener('click', funcSwitchLog);
+    document.querySelector("#logout").addEventListener('click', logOut);
+
 }
+
+//FUNCIONES DE MUESTREO
+function ocultarSecciones(){
+    let sections = document.querySelectorAll('section');
+    for(let i = 0; i < sections.length; i++){
+        sections[i].style.display = 'none';
+    }
+}
+
+function mostrarSeccion(seccion){
+    document.querySelector(`#${seccion}`).style.display = 'block';
+}
+
+//FUNCIONES DE SESION
+function funcLogin(){
+
+    let user = document.querySelector('#txtUserLogin').value;
+    let pass = document.querySelector('#txtPassLogin').value;
+
+    let log = system.login(user, pass);
+    
+    if(log.access){
+        logged = true;
+        document.querySelector("#logout").style.display = 'block';
+
+        ocultarSecciones();
+        switch(log.userType){
+            case 'cliente' : mostrarSeccion('interfazCliente');
+                break;
+            case 'paseador' : mostrarSeccion('interfazPaseador');
+                break;
+        }
+    }
+}
+
+function funcSignup(){
+    let user = document.querySelector('#txtUserSignup').value;
+    let pass = document.querySelector('#txtPassSignup').value;
+    let nombre = document.querySelector('#txtNombreSignup').value;
+    let tamano = document.querySelector('#slcTamanoSignup').value;
+
+    system.signup(user, pass, nombre, tamano);
+}
+
+function logOut(){
+    loged = false;
+    document.querySelector("#logout").style.display = 'none';
+    ocultarSecciones();
+    mostrarSeccion('login');
+}
+
+function funcSwitchLog(){
+    let idBtn = this.getAttribute('id');
+    ocultarSecciones();
+    
+    switch(idBtn){
+        case 'btnToLogin' : mostrarSeccion('login');
+            break;
+        case 'btnToSignup' : mostrarSeccion('signup');
+            break;
+    }
+}
+
+
+
+
+
--- scripts/system.js
+++ scripts/system.js
@@ -1,51 +1,91 @@
 class Sistema{
     constructor(){
         this.paseadores = [
-            new Paseador('0', 'mrodriguez', '1189', 'Martin Rodriguez', 8, 'paseador'),
-            new Paseador('1', 'lpereira', '4521', 'Luciano Pereira', 12, 'paseador'),
-            new Paseador('2', 'mrojas', '7093', 'Martina Rojas', 6, 'paseador'),
-            new Paseador('3', 'lzapata', '3348', 'Leonardo Zapata', 20, 'paseador'),
-            new Paseador('4', 'jpinola', '6217', 'Juan Pinola', 15, 'paseador'),
+            new Paseador('0', 'mrodriguez', 'Mro12', 'Martin Rodriguez', 8, 'paseador'),
+            new Paseador('1', 'lpereira', 'Lp452', 'Luciano Pereira', 12, 'paseador'),
+            new Paseador('2', 'mrojas', 'Mrj93', 'Martina Rojas', 6, 'paseador'),
+            new Paseador('3', 'lzapata', 'Lz334', 'Leonardo Zapata', 20, 'paseador'),
+            new Paseador('4', 'jpinola', 'Jp617', 'Juan Pinola', 15, 'paseador'),
         ];
+
         this.clientes = [
-            new Cliente('crodriguez', '1189', 'Rocky', 3, 'cliente'),
-            new Cliente('mgomez', '4521', 'Luna', 2, 'cliente'),
-            new Cliente('spereira', '7093', 'Simba', 4, 'cliente'),
-            new Cliente('lfernandez', '3348', 'Nala', 3, 'cliente'),
-            new Cliente('vmorales', '6217', 'Max', 4, 'cliente'),
-            new Cliente('tramirez', '9084', 'Milo', 2, 'cliente'),
-            new Cliente('fcastro', '1472', 'Kiara', 3, 'cliente'),
-            new Cliente('mlopez', '3850', 'Toby', 4, 'cliente'),
-            new Cliente('jnavarro', '2765', 'Coco', 1, 'cliente'),
-            new Cliente('dcabrera', '8632', 'Bruno', 3, 'cliente'),
-            new Cliente('mluna', '5631', 'Lola', 4, 'cliente'),
-            new Cliente('rgarcia', '8024', 'Chispa', 1, 'cliente'),
-            new Cliente('ccardozo', '1947', 'Bobby', 4, 'cliente'),
-            new Cliente('jmartinez', '4783', 'Sasha', 3, 'cliente'),
-            new Cliente('adelgado', '6570', 'Rocco', 4, 'cliente'),
-            new Cliente('nfernandez', '3405', 'Mora', 2, 'cliente'),
-            new Cliente('emendez', '7209', 'Zeus', 4, 'cliente'),
-            new Cliente('fpaz', '1332', 'Fiona', 3, 'cliente'),
-            new Cliente('rsilva', '9846', 'Greta', 2, 'cliente'),
-            new Cliente('jrios', '2517', 'Duke', 4, 'cliente'),
+            new Cliente('crodriguez', 'Cr235', 'Rocky', 2, 'cliente'),
+            new Cliente('mgomez', 'Mg352', 'Luna', 2, 'cliente'),
+            new Cliente('spereira', 'Sp368', 'Simba', 4, 'cliente'),
+            new Cliente('lfernandez', 'Lf178', 'Nala', 2, 'cliente'),
+            new Cliente('vmorales', 'Vm261', 'Max', 4, 'cliente'),
+            new Cliente('tramirez', 'Tr908', 'Milo', 2, 'cliente'),
+            new Cliente('fcastro', 'Fc147', 'Kiara', 4, 'cliente'),
+            new Cliente('mlopez', 'Ml385', 'Toby', 4, 'cliente'),
+            new Cliente('jnavarro', 'Jn276', 'Coco', 1, 'cliente'),
+            new Cliente('dcabrera', 'Dc863', 'Bruno', 2, 'cliente'),
+            new Cliente('mluna', 'Ml563', 'Lola', 4, 'cliente'),
+            new Cliente('rgarcia', 'Rg802', 'Chispa', 1, 'cliente'),
+            new Cliente('ccardozo', 'Cc194', 'Bobby', 4, 'cliente'),
+            new Cliente('jmartinez', 'Jm478', 'Sasha', 1, 'cliente'),
+            new Cliente('adelgado', 'Ad657', 'Rocco', 1, 'cliente'),
+            new Cliente('nfernandez', 'Nf340', 'Mora', 2, 'cliente'),
+            new Cliente('emendez', 'Em720', 'Zeus', 4, 'cliente'),
+            new Cliente('fpaz', 'Fp133', 'Fiona', 4, 'cliente'),
+            new Cliente('rsilva', 'Rs984', 'Greta', 1, 'cliente'),
+            new Cliente('jrios', 'Jr251', 'Duke', 4, 'cliente'),
         ];
     }
 
     login(user, pass){
-        let paseadores = this.paseadores;
-        let clientes = this.clientes;
         let allUsers = [...paseadores, ...clientes];
+        let clientes = this.clientes;
+        let paseadores = this.paseadores;
 
         let access = false;
+        let userType;
+
         for(let i = 0; i < allUsers.length; i++){
            if(allUsers[i].usuario === user){
                 if(allUsers[i].pass === pass){
                     access = true;
+                    userType = allUsers[i].rol;
                 }
             }
         }
 
-        console.log(access)
+        return {access, userType}
     }
 
+    signup(user, pass, mascota, tamano){
+        let existe = false;
+        console.log(...paseadores)
+        //let allUsers = [...paseadores, ...clientes];
+
+
+        for(let i = 0; i < allUsers.length; i++){
+            if(allUsers[i].usuario != user){
+                console.log('no existen dos usuarios iguales')
+            }
+        }
+    }
+
+
+
+
+}
+
+function comprobarContraseña(pass){
+    let mayus = false;
+    let minus = false;
+    let num = false;
+
+    for(let i = 0; i < pass.length; i++){
+        if(pass.charAt[i] === pass.charAt[i].toUpperCase()){
+            mayus = true;
+        } else if(pass.charAt[i] === pass.charAt[i].toLowerCase()){
+            minus = true;
+        } else if(pass.charCodeAt(i) > 47 && pass.charCodeAt(i) < 58){
+            num = true;
+        }
+    }
+
+    if(mayus && minus && num){
+        return true;
+    }
 }
